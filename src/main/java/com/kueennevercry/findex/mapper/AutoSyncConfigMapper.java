3d5ef676a98104ef142c32adc8910ae5562327a5
bs8841 <whqortjsgkgk@naver.com>
2025-06-08 19:43:10 +0900

    feat: #33 AutoSyncConfig 매퍼 정의

---
+++ src/main/java/com/kueennevercry/findex/mapper/AutoSyncConfigMapper.java
@@ -0,0 +1,13 @@
+package com.kueennevercry.findex.mapper;
+
+import com.kueennevercry.findex.config.AutoSyncConfig;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AutoSyncConfigMapper {
+
+  @Mapping(target = "indexClassification", source = "indexClassification")
+  @Mapping(target = "indexName", source = "indexName")
+  AutoSyncConfig toDto(AutoSyncConfig config, String indexClassification, String indexName);
+}
