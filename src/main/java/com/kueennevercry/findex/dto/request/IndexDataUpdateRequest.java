776fea02eb4714eaa7a00f650f2842a5a344f6d3
JinsolPark <jinsol8k@gmail.com>
2025-06-09 16:40:31 +0900

    feat: 지수 데이터 수정

--- src/main/java/com/kueennevercry/findex/dto/request/IndexDataUpdateRequest.java
+++ src/main/java/com/kueennevercry/findex/dto/request/IndexDataUpdateRequest.java
@@ -1,10 +1,6 @@
 package com.kueennevercry.findex.dto.request;
 
-import com.kueennevercry.findex.entity.IndexInfo;
-import com.kueennevercry.findex.entity.SourceType;
-
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public record IndexDataUpdateRequest(
         BigDecimal marketPrice,
