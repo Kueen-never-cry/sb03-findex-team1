eff891423b4630f9c2e4daf2791a70852ac747bc
yeokyeong <yeokyeong1004@gmail.com>
2025-06-09 11:33:19 +0900

    fix : integration_tasks 엔티티 필트 오타 수정 & 필드 타입 변경

--- src/main/java/com/kueennevercry/findex/dto/SyncJobDto.java
+++ src/main/java/com/kueennevercry/findex/dto/SyncJobDto.java
@@ -1,18 +1,19 @@
 package com.kueennevercry.findex.dto;
 
-import com.kueennevercry.findex.entity.IntegrationTask;
+import com.kueennevercry.findex.entity.IntegrationJobType;
 import com.kueennevercry.findex.entity.IntegrationResultType;
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 /*연동 작업 DTO */
 @Getter
 public class SyncJobDto {
-private int id;
-private IntegrationTask jobType;
-private int indexInFold;
-private Instant targetDate;
-private String worker;
-private Instant jobTime;
-private IntegrationResultType result;
+
+  private Long id;
+  private IntegrationJobType jobType;
+  private int indexInfoId;
+  private LocalDate targetDate;
+  private LocalDateTime jobTime;
+  private IntegrationResultType result;
 }
