0c356d035cc156a7576237b563d3a033c480abc8
yeokyeong <yeokyeong1004@gmail.com>
2025-06-11 00:00:16 +0900

    feat : 지수 정보 연동 API 추가

--- src/main/java/com/kueennevercry/findex/infra/openapi/DefaultOpenApiClient.java
+++ src/main/java/com/kueennevercry/findex/infra/openapi/DefaultOpenApiClient.java
@@ -22,21 +22,48 @@ public class DefaultOpenApiClient implements OpenApiClient {
   private final OpenApiProperties properties;
   private final ObjectMapper objectMapper;
 
-  @Override
-  public List<IndexDataDto> fetchIndexData(String indexCode, String endpoint, LocalDate from,
-      LocalDate to)
-      throws IOException, URISyntaxException {
-    URI url = buildUrl(indexCode, endpoint, from, to);
-    ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-    return convert(response.getBody());
+  //FIXME : 각 페이지별로 다 가져와야함 총데이터 203849 개 , 최대 페이지 크기 10000 개
+  // TODO : 외부응답 500 에러일때 전역 에러 처리
+  @Override
+  public List<IndexInfoApiResponse> fetchAllIndexData() {
+    ResponseEntity<String> response = restTemplate.getForEntity(rootUrl(), String.class);
+    try {
+      JsonNode root = objectMapper.readTree(response.getBody());
+      JsonNode items = root.path("response").path("body").path("items").path("item");
+
+      return objectMapper.readerForListOf(IndexInfoApiResponse.class).readValue(items);
+    } catch (IOException e) {
+      throw new IllegalStateException("외부 API 응답 JSON 파싱 실패", e);
+    }
   }
 
-  private List<IndexDataDto> convert(String json) throws IOException {
-    JsonNode root = objectMapper.readTree(json);
-    JsonNode items = root.path("response").path("body").path("items").path("item");
+  // TODO : 사용하는곳 없으면 삭제할것
+  private List<IndexDataDto> convert(String json) {
+    try {
+      JsonNode root = objectMapper.readTree(json);
+      JsonNode items = root.path("response").path("body").path("items").path("item");
+      return objectMapper.readerForListOf(IndexDataDto.class).readValue(items);
+    } catch (IOException e) {
+      throw new IllegalStateException("외부 API 응답 JSON 파싱 실패", e);
+    }
+  }
 
-    return objectMapper.readerForListOf(IndexDataDto.class).readValue(items);
+
+  /* FIXME : 리팩토링할때 buildUrl(String indexCode, String endpoint, LocalDate from, LocalDate to) 메소드와 공통작업은 빼서 다시 구현 */
+  private URI rootUrl() {
+    try {
+      String serviceKey = properties.getApiKey();
+
+      String base = properties.getBaseUrl() + "/getStockMarketIndex"
+          + "?serviceKey=" + serviceKey
+          + "&resultType=json";
+      // + 기호는 인코딩에서 제외되기 때문에 미리 변환하고
+      // URI 클래스를 사용하면 URL 전송 할 때 문자열 그대로 날아가는 것이 아닌, 한 번 인코딩을 해서 보내준다
+      return new URI(base.replace("+", "%2B"));
+    } catch (URISyntaxException e) {
+      throw new IllegalArgumentException("잘못된 URI 형식입니다: " + e.getInput(), e);
+    }
   }
 
   private URI buildUrl(String indexCode, String endpoint, LocalDate from, LocalDate to)
--- src/main/java/com/kueennevercry/findex/service/SyncJobService.java
+++ src/main/java/com/kueennevercry/findex/service/SyncJobService.java
@@ -1,26 +1,87 @@
 package com.kueennevercry.findex.service;
 
 import com.kueennevercry.findex.dto.CursorPageResponseSyncJobDto;
+import com.kueennevercry.findex.dto.SyncJobDto;
 import com.kueennevercry.findex.dto.request.SyncJobParameterRequest;
+import com.kueennevercry.findex.entity.IndexInfo;
+import com.kueennevercry.findex.entity.IntegrationJobType;
+import com.kueennevercry.findex.entity.IntegrationResultType;
+import com.kueennevercry.findex.entity.IntegrationTask;
+import com.kueennevercry.findex.entity.SourceType;
+import com.kueennevercry.findex.infra.openapi.IndexInfoApiResponse;
 import com.kueennevercry.findex.infra.openapi.OpenApiClient;
-import com.kueennevercry.findex.repository.SyncJobRepository;
+import com.kueennevercry.findex.mapper.IntegrationTaskMapper;
+import com.kueennevercry.findex.repository.IndexInfoRepository;
+import com.kueennevercry.findex.repository.IntegrationTaskRepository;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SyncJobService {
 
-  @Autowired
-  private final SyncJobRepository syncJobRepository;
+  private final IntegrationTaskRepository integrationTaskRepository;
+  private final IndexInfoRepository indexInfoRepository;
+  private final IntegrationTaskMapper integrationTaskMapper;
   private final OpenApiClient openApiClient;
-  // FIXME : 어차피 이번 프로젝트에서는 /getStockMarketIndex 만 사용하므로 'application.yaml' > 'base-url' 환경변수에 아예 넣어서 사용할것.
-  private String STOCK_INDEX_ENDPOINT = "/getStockMarketIndex";
 
   public CursorPageResponseSyncJobDto findAllByParameters(
       SyncJobParameterRequest syncJobParameterRequest) {
 
-    return syncJobRepository.findAllByParameters(syncJobParameterRequest);
+    return integrationTaskRepository.findAllByParameters(syncJobParameterRequest);
   }
+
+  public List<SyncJobDto> syncIndexInfo() {
+    // 1. 순수 openApi에서 가져온  데이터
+    List<IndexInfoApiResponse> indexDataList = openApiClient.fetchAllIndexData();
+
+    //2. indexInfo에 이미 데이터 있으면 바로 integrationTask 데이터 삽입, 없으면 indexInfo 데이터 생성 후 integrationTask 데이터 삽입
+    List<IntegrationTask> integrationTaskList = new ArrayList<>();
+    indexDataList.forEach(indexInfoFromApi -> {
+      Optional<IndexInfo> indexInfo = indexInfoRepository.findByIndexNameAndIndexClassification(
+          indexInfoFromApi.indexName(), indexInfoFromApi.indexClassification());
+      if (indexInfo.isPresent()) {
+        integrationTaskList.add(this.buildIntegrationTaskEntity(indexInfo.get()));
+      } else {
+        IndexInfo createdIndexInfo = this.createIndexInfo(indexInfoFromApi);
+        integrationTaskList.add(this.buildIntegrationTaskEntity(createdIndexInfo));
+      }
+    });
+    List<IntegrationTask> createdIntegrationTaskList = this.createIntegrationTasks(
+        integrationTaskList);
+
+    return createdIntegrationTaskList.stream().map(integrationTaskMapper::toSyncJobDto).toList();
+  }
+
+  private IndexInfo createIndexInfo(IndexInfoApiResponse indexInfoFromApi) {
+    IndexInfo newIndexInfo = IndexInfo.builder()
+        .indexClassification(indexInfoFromApi.indexClassification())
+        .indexName(indexInfoFromApi.indexName())
+        .employedItemsCount(indexInfoFromApi.employedItemsCount())
+        .basePointInTime(indexInfoFromApi.basePointInTime())
+        .baseIndex(indexInfoFromApi.baseIndex()).sourceType(SourceType.OPEN_API).favorite(false)
+        .build();
+
+    return indexInfoRepository.save(newIndexInfo);
+  }
+
+  private IntegrationTask buildIntegrationTaskEntity(IndexInfo indexInfo) {
+    return IntegrationTask.builder()
+        .jobType(IntegrationJobType.INDEX_INFO)
+        .targetDate(null)
+        .worker(SourceType.OPEN_API.name())
+        .jobTime(Instant.now())
+        .result(IntegrationResultType.SUCCESS)  // TODO : success를 default로 두는게 맞나? -> 추후 수정
+        .indexInfo(indexInfo)
+        .build();
+  }
+
+  private List<IntegrationTask> createIntegrationTasks(List<IntegrationTask> integrationTaskList) {
+    return integrationTaskRepository.saveAll(integrationTaskList);
+  }
+
 }
