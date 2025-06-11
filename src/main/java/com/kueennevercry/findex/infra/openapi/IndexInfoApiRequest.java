d5a320a835f2d4a3a087fabbfef18f987434e0a5
yeokyeong <yeokyeong1004@gmail.com>
2025-06-11 12:14:38 +0900

    add : IndexInfoApiRequest DTO 추가

---
+++ src/main/java/com/kueennevercry/findex/infra/openapi/IndexInfoApiRequest.java
@@ -0,0 +1,20 @@
+package com.kueennevercry.findex.infra.openapi;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class IndexInfoApiRequest {
+
+  private int pageNo;// 페이지번호
+
+  private int numOfRows; // 한 페이지 결과 수
+
+  private String beginBasDt; // 기준일자가 검색값보다 크거나 같은 데이터를 검색
+
+  private String endBasDt; // 기준일자가 검색값보다 작은 데이터를 검색
+
+}
