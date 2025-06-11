8def4f63d01b384acf2028132bae730f27806e73
yeokyeong <yeokyeong1004@gmail.com>
2025-06-11 15:04:41 +0900

    refactor : Open API 관련 DTO 디렉토리 이동

--- src/main/java/com/kueennevercry/findex/dto/request/IndexInfoApiRequest.java
+++ src/main/java/com/kueennevercry/findex/dto/request/IndexInfoApiRequest.java
@@ -13,6 +13,8 @@ public class IndexInfoApiRequest {
 
   private int numOfRows; // 한 페이지 결과 수
 
+  private String basDt; // 검색값과 기준일자가 일치하는 데이터를 검색 ex)20250610
+
   private String beginBasDt; // 기준일자가 검색값보다 크거나 같은 데이터를 검색
 
   private String endBasDt; // 기준일자가 검색값보다 작은 데이터를 검색
