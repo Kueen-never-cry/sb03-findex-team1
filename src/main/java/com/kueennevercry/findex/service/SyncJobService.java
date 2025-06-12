5103a4a22a596648152faaf0c890022aef917b86
NINE <99791512+NINE-J@users.noreply.github.com>
2025-06-12 22:08:33 +0900

    fix: 지수 데이터 연동 수정

--- src/main/java/com/kueennevercry/findex/service/SyncJobService.java
+++ src/main/java/com/kueennevercry/findex/service/SyncJobService.java
@@ -112,8 +112,10 @@ public class SyncJobService {
     for (IndexInfo indexInfo : indexInfos) {
       try {
         List<IndexInfoApiResponse> responses = openApiClient
-            .fetchAllIndexDataByNameAndDateRange(indexInfo.getIndexName(), from,
-                to);
+            .fetchAllIndexDataByNameAndDateRange(indexInfo.getIndexName(), from, to)
+            .stream()
+            .filter(res -> res.indexClassification().equals(indexInfo.getIndexClassification()))
+            .toList();
 
         // 날짜별로 그룹화
         Map<LocalDate, List<IndexInfoApiResponse>> byDate =
